473f7adbf1735514d8eefdeca32b599d93686496
AhmedAli-cp <ahmed.ali@cloudprimero.com>
2022-03-01 19:53:53 +0500

    Initial fastApi app with converters

---
+++ DocumentExtractor/file_conversion/converters/__init__.py
@@ -0,0 +1,3 @@
+from .pdftoimage import PDF2ImageConverter as PDFConverter
+from .docx2image import Document2ImageConverter as DocConverter
+from .image2image import Image2ImageConverter as ImageConverter
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, UploadFile,  Request
+import uvicorn
+from DocumentExtractor.file_conversion.format_conversion_manager import  FormatConversionManager
+from DocumentExtractor import utils
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {'path': ''}
+
+
+@app.post("/ImageExtraction")
+def upload_file(file: UploadFile):
+    """
+    :type file: Image, docx, PDF
+    """
+    # Creating temp file
+    doc_path = utils.save_upload_file_tmp(file)
+
+    fcm = FormatConversionManager()
+
+    # Get image path/list
+    img_list = fcm(doc_path)
+    # print(img_list)
+
+    return {"file": {"File Name": file.filename, "File Type": file.content_type}}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
