5a1f143b8ec6d1cc05a531290b28f59bb8aa2700
AhmedAli-cp <ahmed.ali@cloudprimero.com>
2022-03-02 18:37:39 +0500

    update utils

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, UploadFile,  Request
-from utils.document_extractor.file_conversion.format_conversion_manager import FormatConversionManager
+from fastapi import FastAPI, UploadFile, Request
 from utils import utils
+from utils.document_extractor.file_conversion.format_conversion_manager import FormatConversionManager
+
+# import uvicorn
 
 app = FastAPI()
 
@@ -15,17 +17,13 @@ def upload_file(file: UploadFile):
     """
     :type file: Image, docx, PDF
     """
-    # Creating temp file
-    doc_path = utils.save_upload_file_tmp(file)
 
-    fcm = FormatConversionManager()
+    # Handle Upload file
+    img = utils.handle_upload_file(file, FormatConversionManager())
 
-    # Get image path/list
-    img_list = fcm(doc_path)
-    # print(img_list)
+    #print(img)
 
     return {"file": {"File Name": file.filename, "File Type": file.content_type}}
-
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="127.0.0.1", port=9000)
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ from fastapi import UploadFile
 import shutil
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+from typing import Callable
 
 
 def save_upload_file_tmp(file: UploadFile) -> Path:
@@ -13,3 +14,16 @@ def save_upload_file_tmp(file: UploadFile) -> Path:
     finally:
         file.file.close()
     return tmp_path
+
+
+def handle_upload_file(
+        upload_file: UploadFile, handler: Callable[[Path], str]
+) -> str:
+    tmp_path = save_upload_file_tmp(upload_file)
+    img = ""
+    try:
+        img = handler(tmp_path)
+
+    finally:
+        tmp_path.unlink()  # Delete the temp file
+        return img
